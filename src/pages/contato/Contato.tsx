c03353e6d0aae71ae90ff4a888caca3a3b0bbdf1
angelmcastro <angela.mendonca97@hotmail.com>
2022-04-29 18:22:43 -0300

    atualizando contato

---
+++ src/pages/contato/Contato.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Sobre.css';
+
+
+function Contato() {
+    return (
+        <>
+            <div>
+                <h1 className='titulo' >Quem somos nós?</h1>
+                <h3 className='center'>Um e-commerce de moda voltado a sustentabilidade e praticidade para aquisição. <br />
+                Formado após uma inniciativa de jovens desenvolvedores preocupados com o mundo.
+                </h3>
+            </div>
+            <div className='pad'>
+                <h2 className='titulo'>Entre em contato conosco:</h2>
+                <h3 className='center'>
+                    muskiinmp@gmail.com
+                    <p></p>
+                
+                </h3>
+            </div>
+        </>
+    );
+}
+
+export default Contato;
