553cd17183f36eaf19e0a1c038f931f2f44cfb99
ViniciusNeivas <91344925+ViniciusNeivas@users.noreply.github.com>
2022-04-29 15:52:44 -0300

    Atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/home/Home'
-
+import Navbar from './components/static/navbar/Navbar'
+import Footer from './components/static/footer/Footer'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login/Login'
 
 function App() {
-  return (
-   <Home />
-  );
+  return(
+
+    
+      <Router>
+        <Navbar />
+        <div style={{ minHeight: '100vh' }}>
+          <Routes> // Antigo Switch
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home" element={<Home />} />
+  
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    );
+    
+
 }
 
 export default App;
